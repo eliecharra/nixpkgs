144b1d3bd407fa6af2af1e6cf6c89eae4a32f68e
Elie <elie.charra@snyk.io>
2021-11-29 16:54:30 +0100

    driftctl: Better way to disable telemetry

--- pkgs/applications/networking/cluster/driftctl/default.nix
+++ pkgs/applications/networking/cluster/driftctl/default.nix
@@ -13,19 +13,8 @@ buildGoModule rec {
 
   vendorSha256 = "sha256-aaJ5fpS+BiVq1K8OxN+/CBD96wy3flGDhch8O2ACIh8=";
 
-  postUnpack = ''
-
-    # Disable check for latest version and telemetry, which are opt-out.
-    # Making it out-in is quite a job, and why bother?
-    find -name '*.go' \
-      | xargs sed -i 's,https://2lvzgmrf2e.execute-api.eu-west-3.amazonaws.com/,https://0.0.0.0/,g'
-
-    # and remove corresponding flags from --help, so things look tidy.
-    find -name driftctl.go | \
-      xargs sed -i -e '/("no-version-check"/ d'  -e '/("disable-telemetry"/ d'
-  '';
-
   ldflags = [
+    "-X github.com/cloudskiff/driftctl/build.enableUsageReporting=false"
     "-X github.com/cloudskiff/driftctl/build.env=release"
     "-X github.com/cloudskiff/driftctl/pkg/version.version=v${version}"
   ];
